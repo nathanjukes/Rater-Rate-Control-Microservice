03a9cd0579bb002410b5cb7726fd0c396e951178
nathanjukes <njukes1@gmail.com>
2023-11-29 17:09:51 +0000

    add api processing controller tests

--- src/test/java/RateControlTests/Controllers/ApiProcessingControllerTest.java
+++ src/test/java/RateControlTests/Controllers/ApiProcessingControllerTest.java
@@ -3,6 +3,7 @@ package RateControlTests.Controllers;
 import RateControl.Controllers.ApiProcessingController;
 import RateControl.Exceptions.InternalServerException;
 import RateControl.Exceptions.UnauthorizedException;
+import RateControl.Models.ApiRequest.ApiRequest;
 import RateControl.Models.Auth.Auth;
 import RateControl.Models.Org.Org;
 import RateControl.Security.SecurityService;
@@ -17,9 +18,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ApiProcessingControllerTest {
@@ -41,10 +43,32 @@ public class ApiProcessingControllerTest {
         testOrg.setId(UUID.randomUUID());
         auth = new Auth("test");
 
+        when(securityService.getAuthedOrg()).thenReturn(Optional.of(testOrg));
+        when(securityService.getAuthToken(any())).thenReturn(Optional.of(auth));
     }
 
     @Test
-    public void testProcessRequest() {
+    public void testProcessRequest() throws InternalServerException, UnauthorizedException, ExecutionException, InterruptedException {
+        String apiKey = "apikeytest";
+        String apiPath = "/api/test";
+        UUID userId = UUID.randomUUID();
+        ApiRequest apiRequest = new ApiRequest(apiKey, apiPath, userId);
 
+        apiProcessingController.processRequest(apiRequest, null);
+
+        verify(apiProcessingService, times(1)).processRequest(eq(apiRequest));
+        verify(apiProcessingService, times(1)).getApiStatus(eq(apiRequest), eq(true), eq(auth));
+    }
+
+    @Test
+    public void testGetApiStatus() throws InternalServerException, UnauthorizedException, ExecutionException, InterruptedException {
+        String apiKey = "apikeytest";
+        String apiPath = "/api/test";
+        UUID userId = UUID.randomUUID();
+        ApiRequest apiRequest = new ApiRequest(apiKey, apiPath, userId);
+
+        apiProcessingController.getApiStatus(apiRequest, null);
+
+        verify(apiProcessingService, times(1)).getApiStatus(eq(apiRequest), eq(false), eq(auth));
     }
 }
